f56d858b1c43f6967af24701b55388f0c2e0f468
ChristalCamillo <christal.b.camillo@gmail.com>
2023-10-17 16:48:28 -0300

    promisses

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('olá mundo');
+import chalk from 'chalk';
+import fs from 'fs';
+
+function trataErro(erro){
+	throw new Error(chalk.red(erro.code, 'ARQUIVO INEXISTENTE'));
+}
+
+function pegaArquivo(path) {
+	const encoding = 'utf-8';
+	fs.readFile(path, encoding, (erro, texto) => {
+		if(erro) {
+			trataErro(erro);
+		}
+		console.log(chalk.green(texto));
+	})
+}
+
+pegaArquivo('./arquivos/texto.md');
