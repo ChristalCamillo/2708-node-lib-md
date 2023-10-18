ae7eab43da32f0b2bc5f800d885be50a288f6d6c
ChristalCamillo <christal.b.camillo@gmail.com>
2023-10-18 15:39:30 -0300

    aula3

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
-function trataErro(erro){
+function trataErro(erro) {
 	throw new Error(chalk.red(erro.code, 'ARQUIVO INEXISTENTE'));
 }
 
-function pegaArquivo(path) {
-	const encoding = 'utf-8';
-	fs.readFile(path, encoding, (erro, texto) => {
-		if(erro) {
-			trataErro(erro);
-		}
+//async/await
+//finally opcional, bloco de codigo que sempre será executado
+async function pegaArquivo(path) {
+	try {
+		const encoding = 'utf-8';
+		const texto = await fs.promises.readFile(path, encoding)
 		console.log(chalk.green(texto));
-	})
+	} catch (erro) {
+		trataErro(erro);
+	} finally {
+		console.log(chalk.yellow('operação concluída'));
+	}
 }
 
+
+
+
+// promessas com then
+// function pegaArquivo(path){
+// 	const encoding = 'utf-8';
+// 	funçoes encadeadas após o promise
+// 	fs.promises
+// 	.readFile(path, encoding)
+// 	.then((texto) => console.log(chalk.green(texto)))
+// 	.catch(trataErro)
+// }
+
 pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/tex.md');
