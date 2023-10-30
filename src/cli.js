d19763e01c885969081ad6c80c0a557ea27f8705
ChristalCamillo <christal.b.camillo@gmail.com>
2023-10-30 17:40:51 -0300

    lista de urls

--- src/cli.js
+++ src/cli.js
@@ -5,25 +5,34 @@
 import pegaArquivo from './index.js';
 import chalk from 'chalk';
 import fs from 'fs';
+import listaValidada from './http-validacao.js';
 
 const caminho = process.argv;
 
 // o identificador é uma propriedade criada para nomear os arquivos que são links
 //iniciamos a var vazia para, em situ de leitura de arquivo, não aparecer "undefined"
-function imprimeLista(resultado, identificador = '') {
-  console.log(
+function imprimeLista(valida, resultado, identificador = '') {
+	if(valida){
+		  console.log(
+    chalk.yellow('lista validada'),
+    chalk.black.bgGreen(identificador),
+    listaValidada(resultado));
+	} else {
+		  console.log(
     chalk.yellow('lista de links'),
     chalk.black.bgGreen(identificador),
     resultado);
+	}
 }
 
 async function processaTexto(argumentos) {
 	const caminho = argumentos[2];
+	//dessa forma, valida será sempre true or false
+	const valida = argumentos[3] === '--valida';
 
 	try {
 		//qualquer caminho incorreto que for recebido vai gerar erro
 		fs.lstatSync(caminho);
-
 	} catch (erro) {
 		if (erro.code === 'ENOENT') {
 			console.log('arquivo ou diretório não existe');
@@ -34,12 +43,12 @@ async function processaTexto(argumentos) {
 
 	if (fs.lstatSync(caminho).isFile()) {
 		const resultado = await pegaArquivo(argumentos[2]);
-		imprimeLista(resultado);
+		imprimeLista(valida, resultado);
 	} else if (fs.lstatSync(caminho).isDirectory()) {
 		const arquivos = await fs.promises.readdir(caminho)
 		arquivos.forEach(async (nomeDeArquivo) => {
 			const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
-			imprimeLista(lista, nomeDeArquivo)
+			imprimeLista(valida, lista, nomeDeArquivo)
 		})
 	}
 }
